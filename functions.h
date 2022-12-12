8deb21cd857a9220a4ffe454ace63f584d6db5e9
jerome-ha <jerome.ha@ensea.fr>
2022-12-12 17:32:11 +0100

    First commit

--- functions.h
+++ functions.h
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <time.h>
 #define Message "Welcome to ENSEA Tiny Shell.\n Pour quitter, tapez 'exit'.\n"
-#define PROMPT "enseash % "
-#define Message1 "Today is what happened to yesterday.\n"
+#define PROMPT "enseash "
+#define PROMPT1 " %"
+#define Message1 "[Today is what happened to yesterday.]"
 #define Message2 "ERROR.\n"
--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 int main(){
 	write(STDOUT_FILENO, Message, strlen(Message));
 	write(STDOUT_FILENO, PROMPT, strlen(PROMPT));
-	
+	write(STDOUT_FILENO, PROMPT1, strlen(PROMPT1));
 	char test[128];
 	
 	time_t rawtime ;
@@ -14,17 +14,22 @@ int main(){
 	while(1){
 		read(STDIN_FILENO,test,128);
 		if (strcmp(test,"fortune")==10){
-			write(STDOUT_FILENO, Message1, strlen(Message1));
+			write(STDOUT_FILENO, PROMPT, strlen(PROMPT));
+			write(STDOUT_FILENO, Message1 , strlen( Message1 ));
+			write(STDOUT_FILENO, PROMPT1, strlen(PROMPT1));
 		}
 		else if (strcmp(test,"date")==10){
-			write(STDOUT_FILENO, asctime(timeinfo), strlen(asctime(timeinfo)));
+			write(STDOUT_FILENO, PROMPT, strlen(PROMPT));
+			write(STDOUT_FILENO, "[", strlen("["));
+			write(STDOUT_FILENO, asctime(timeinfo), strlen( asctime(timeinfo)));
+			write(STDOUT_FILENO, "] %", strlen("] %"));
 		}
 			
 		else if (strcmp(test,"exit")==10){
 			write(STDOUT_FILENO, "Bye bye...\n", strlen("Bye bye...\n"));
 			break;
 		}
-		write(STDOUT_FILENO, PROMPT, strlen(PROMPT));
+		
 	}
 	
 	return 0;}
